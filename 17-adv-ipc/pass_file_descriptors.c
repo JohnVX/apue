cc3fbc2340f838c65420822ddf73fc37ee9c20af
JohnVX <wswbpf@163.com>
2017-07-23 14:28:53 +0800

    ok

---
+++ 17-adv-ipc/pass_file_descriptors.c
@@ -0,0 +1,6 @@
+#include "apue.h"
+
+int send_fd(int fd, int fd_to_send);
+int send_err(int fd, int status, const char *errmsg);
+int recv_fd(int fd, ssize_t (*userfunc)(int, const void *, size_t));
+
